ac926234ca4466bf61ee83342fea31ca598d51a0
Krisnatagoras <50491859+Krisnatagoras@users.noreply.github.com>
2021-03-21 15:18:22 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,25 +1,30 @@
-class minecraft {
-  file {‘/opt/minecraft’:
+ass minecraft (
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+  $install_dir = '/opt/minecraft'
+){
+  file { $install_dir:
     ensure => directory,
   }
-  file {‘/opt/minecraft/server.jar’:
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
-    source      => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    source => $url,
   }
-  package {‘java’:
+
+  package {'java':
     ensure => present,
   }
-  file {‘/opt/minecraft/eula.txt’:
+  file {"${install_dir}/eula.txt":
     ensure => file,
-    content => ‘eula=true’
+    content => 'eula=true'
   }
   file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    content => epp(‘minecraft/minecraft.service.epp’, {
+      install_dir => $install_dir,
+    }),
   }
-  service { ‘minecraft’:
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }
 }
-
